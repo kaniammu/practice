c046a2ffb37df11f1168a3e1de455ec0f87b17dc
kaniammu <142822940+kaniammu@users.noreply.github.com>
2023-09-04 10:01:14 +0530

    Add files via upload

---
+++ ByteDDemo.java
@@ -0,0 +1,13 @@
+class ByteDDemo
+{
+  public static void main(String[] args)
+    {
+       //byte b = 127;
+       //byte b = -128;
+       //byte b = 128;
+       //byte b = -129;
+       //byte b =; 12.25
+       byte b = true;
+       System.out.print("The value of b = " + b);
+     }
+}
